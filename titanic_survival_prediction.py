1670d1935fbf3ccf1f4959bd9709f34fdb23699e
adam.sanche <hooberschmit@gmail.com>
2019-02-07 17:59:44 -0700

    sexy boys

--- titanic_survival_prediction.py
+++ titanic_survival_prediction.py
@@ -2,24 +2,39 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 titanic_file_path = './titanic/train.csv'
 
 titanic_data = pandas.read_csv(titanic_file_path)
 
+sexes = array(titanic_data['Sex'])
+
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(sexes)
+
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
 # todo one-hot encode Sex
 features = [
-    'Age', 'Pclass'
+    'Age', 'Pclass', 'Sex'
 ]
 
 titanic_data = titanic_data.fillna(method='ffill')
 
 X = titanic_data[features]
+X.Sex = onehot_encoded
 y = titanic_data.Survived
 
 # Split into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
-
 
 rf_model = RandomForestClassifier(random_state=1)
 rf_model.fit(train_X, train_y)
