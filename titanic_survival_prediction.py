4c20475156235f625e78649b42a90921a9d9dacf
Sohail Rajabi <srajabi@gmail.com>
2019-02-07 18:02:58 -0700

    submission section filled out

--- titanic_survival_prediction.py
+++ titanic_survival_prediction.py
@@ -3,16 +3,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
 
+# TESTING AND MODEL CREATION
 titanic_file_path = './titanic/train.csv'
 
 titanic_data = pandas.read_csv(titanic_file_path)
+titanic_data = titanic_data.fillna(method='ffill')
 
 # todo one-hot encode Sex
 features = [
     'Age', 'Pclass'
 ]
 
-titanic_data = titanic_data.fillna(method='ffill')
 
 X = titanic_data[features]
 y = titanic_data.Survived
@@ -29,3 +30,20 @@ score = rf_model.score(train_X, train_y)
 print(score)
 print(y_predicted)
 print(val_y)
+
+
+# SUBMISSION
+test_file_path = './titanic/test.csv'
+
+test_data = pandas.read_csv(test_file_path)
+test_data = test_data.fillna(method='ffill')
+
+X_submit = test_data[features]
+rf_model.fit(X, y) # use entire dataset for training
+
+test_predictions = rf_model.predict(X_submit)
+
+output = pandas.DataFrame({'PassengerId' : test_data.PassengerId,
+                           'Survived' : test_predictions})
+
+output.to_csv('submission.csv', index=False)
