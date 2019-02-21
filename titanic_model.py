222b0fa90ff75b7aedd80307c02827f162c0629f
Kyle Hayward <kyle@Kyles-MacBook-Pro.local>
2019-02-20 22:03:55 -0700

    cleanup

--- titanic_model.py
+++ titanic_model.py
@@ -5,7 +5,6 @@ import seaborn
 import matplotlib
 import matplotlib.pyplot as pyplot
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
